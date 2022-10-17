26fb5b8a2389a95b89a9a171aa1fa0876dac5e4b
pooyan <thepooyan@gmail.com>
2022-10-17 18:52:47 +0330

    font awsome added to prj

--- components/Msg.tsx
+++ components/Msg.tsx
@@ -1,13 +1,16 @@
+import { faReply } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styles from './Msg.module.scss'
 
-const Msg = ({id, isMe=true, children, time, user, isReply=false}) => {
+type PropsType = {id: number, isMe: boolean, children:any, time: string, user:string, isReply: {id:number, user:string}|undefined}
+const Msg = ({id, isMe=true, children, time, user, isReply=undefined}:PropsType) => {
   return (
-    <div className={isMe?styles.me:styles.others} id={id}>
+    <div className={isMe?styles.me:styles.others} id={id.toString()}>
         <div className={styles.icon} data-user={user}></div>
         <div className={styles.content} data-time={time}>
             <span>{children}</span>
-            <i>R</i>
+            <FontAwesomeIcon icon={faReply}></FontAwesomeIcon>
             {isReply && <a href={`#${isReply.id}`}>{isReply.user}</a>}
         </div>
     </div>
--- components/Reply.tsx
+++ components/Reply.tsx
@@ -1,13 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styles from './Reply.module.scss'
+import { faChevronUp, faClose } from '@fortawesome/free-solid-svg-icons'
 
 const Reply = ({getDownActive=false}) => {
     return (
         <div className={styles.wrap}>
-            <span className={`${styles.getDown} ${getDownActive?styles.active:''}`}>D</span>
+            <span className={`${styles.getDown} ${getDownActive?styles.active:''}`}><FontAwesomeIcon icon={faChevronUp}></FontAwesomeIcon></span>
             <div className={`${styles.reply} ${styles.closed}`}>
                 <div></div>
-                <i>close</i>
+                <FontAwesomeIcon icon={faClose}></FontAwesomeIcon>
             </div>
         </div>
     )
